117b36c9db4c36a253b145db4996dd71743b2d63
nng32 <longcaster19@gmail.com>
2022-11-15 21:31:15 -0800

    Create Combatant interface and Vector2D placeholder class

--- src/main/java/ca/bcit/comp2522/termproject/td/Combatant.java
+++ src/main/java/ca/bcit/comp2522/termproject/td/Combatant.java
@@ -7,5 +7,62 @@ package ca.bcit.comp2522.termproject.td;
  * @version 0.1
  */
 public interface Combatant {
+    /**
+     * Returns the name of the Combatant.
+     *
+     * @return the name as a String
+     */
+    String getName();
 
+    /**
+     * Moves the Combatant to a specific location.
+     *
+     * @param destination the location to move to
+     */
+    void moveTo(Vector2D destination);
+
+    /**
+     * Returns the current location of the Combatant.
+     *
+     * @return the current location as a Vector2D.
+     */
+    Vector2D getLocation();
+
+    /**
+     * Adds or subtracts health from the Combatant.
+     *
+     * @param delta the amount of add or subtract as an int
+     */
+    void changeHealth(int delta);
+
+    /**
+     * Attacks another Combatant.
+     *
+     * @param target the Combatant to attack
+     */
+    void attack(Combatant target);
+
+    /**
+     * Calculates damage resulting from a combat initiation. This is subject to probability such as with accuracy.
+     *
+     * @param weapon the Weapon used for the attack
+     * @return the damage that may result from the attack
+     */
+    int calculateDamage(Weapon weapon);
+
+    /**
+     * Calculates maximum damage resulting from a combat initiation. This assumes all bullets hit the target.
+     *
+     * @param weapon the Weapon used for the attack
+     * @return the total damage that may result from the attack
+     */
+    int calculateMaxDamage(Weapon weapon);
+
+    /**
+     * Reduces the Combatant's health based on the attacker's weapon's stats and distance from the attacker.
+     *
+     * @param distance the distance of the attack, in grid squares
+     * @param weapon the Weapon used for the attack
+     */
+    void receiveAttack(int distance, Weapon weapon);
 }
---
+++ src/main/java/ca/bcit/comp2522/termproject/td/Vector2D.java
@@ -0,0 +1,11 @@
+package ca.bcit.comp2522.termproject.td;
+
+/**
+ * Represents a two-dimensional vector.
+ *
+ * @author Nathan
+ * @version 0.1
+ */
+public class Vector2D {
+
+}
