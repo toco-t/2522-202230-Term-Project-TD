1f7d65d12a76c30517fae10b0bcc94a9c7a9fc62
nng32 <longcaster19@gmail.com>
2022-11-15 21:53:02 -0800

    Add Affiliation and ArmourType enums

---
+++ src/main/java/ca/bcit/comp2522/termproject/td/Affiliation.java
@@ -0,0 +1,12 @@
+package ca.bcit.comp2522.termproject.td;
+
+/**
+ * Represents an object's alignment with a particular faction.
+ *
+ * @author Nathan
+ * @version 0.1
+ */
+public enum Affiliation {
+    PLAYER,
+    ENEMY
+}
---
+++ src/main/java/ca/bcit/comp2522/termproject/td/ArmourType.java
@@ -0,0 +1,14 @@
+package ca.bcit.comp2522.termproject.td;
+
+/**
+ * Represents different types of armour. Used in damage calculations to determine weakness/strength to a specific type.
+ *
+ * @author Nathan
+ * @version 0.1
+ */
+public enum ArmourType {
+    DEFAULT,
+    LIGHT,
+    ERA,
+    COMPOSITE
+}
