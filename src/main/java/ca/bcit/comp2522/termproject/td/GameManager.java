a36fc6bab5e7d9a47ecc5929e2d730ed1ef9258e
toco <ttachibana@my.bcit.ca>
2022-11-17 23:16:01 -0800

    Implement GameManager class

---
+++ src/main/java/ca/bcit/comp2522/termproject/td/GameManager.java
@@ -0,0 +1,77 @@
+package ca.bcit.comp2522.termproject.td;
+
+import ca.bcit.comp2522.termproject.td.map.GameMap;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+/**
+ * Game manager.
+ *
+ * @author Toco Tachibana
+ * @version 0.1
+ */
+public class GameManager {
+
+    private final ArrayList<Combatant> playerUnits;
+    private final ArrayList<Combatant> enemyUnits;
+    private GameMap map;
+    private Combatant selectedUnit;
+
+    /**
+     * Constructs an object of type GameManager.
+     */
+    public GameManager() {
+        this.playerUnits = new ArrayList<>();
+        this.enemyUnits = new ArrayList<>();
+
+        createGameMap();
+    }
+
+    /**
+     * Loads all the units to the specified list.
+     *
+     * @param lists the specified list, an ArrayList of Combatant
+     * @param units variable length of units, an array
+     * @throws IllegalArgumentException when units is empty
+     */
+    public void loadUnits(final ArrayList<Combatant> lists, final Combatant... units) {
+        if (units == null) {
+            throw new IllegalArgumentException("Units cannot be null...");
+        }
+
+        Collections.addAll(lists, units);
+    }
+
+    /**
+     * Creates a GameMap.
+     */
+    public void createGameMap() {
+        this.map = new GameMap();
+    }
+
+    /**
+     * Selects the specified Unit to play.
+     *
+     * @param unit the specified Unit to play, a Combatant
+     */
+    public void selectUnit(final Combatant unit) {
+        if (playerUnits.contains(unit)) {
+            this.selectedUnit = unit;
+        }
+    }
+
+    /**
+     * Moves the selected Unit to the location at the specified coordinates.
+     *
+     * @param coordinates destination for this move, a Vector2D
+     * @throws IllegalArgumentException when the specified coordinates is null
+     */
+    public void moveUnit(final Vector2D coordinates) throws IllegalArgumentException {
+        if (coordinates == null) {
+            throw new IllegalArgumentException("Coordinates cannot be null...");
+        } else {
+            selectedUnit.moveTo(coordinates);
+        }
+    }
+}
--- src/main/java/ca/bcit/comp2522/termproject/td/map/GameMap.java
+++ src/main/java/ca/bcit/comp2522/termproject/td/map/GameMap.java
@@ -8,9 +8,12 @@ import java.util.ArrayList;
  * @author Toco Tachibana
  * @version 0.1
  */
-public class Map {
+public class GameMap {
 
     private enum Weather { SUNNY, OVERCAST, STORMY, RAINING }
+
+    private static final Weather DEFAULT_WEATHER = Weather.SUNNY;
+    private static final boolean DEFAULT_CONDITION = false;
 
     private final ArrayList<Tile> grids;
     private final Weather weather;
@@ -22,10 +25,17 @@ public class Map {
      * @param weather     weather condition of this Map, an enum Weather
      * @param isNighttime a boolean
      */
-    public Map(final Weather weather, final boolean isNighttime) {
+    public GameMap(final Weather weather, final boolean isNighttime) {
         this.grids = new ArrayList<>();
 
         this.weather = weather;
         this.isNighttime = isNighttime;
+    }
+
+    /**
+     * Constructs an object of type Map, no params.
+     */
+    public GameMap() {
+        this(DEFAULT_WEATHER, DEFAULT_CONDITION);
     }
 }
